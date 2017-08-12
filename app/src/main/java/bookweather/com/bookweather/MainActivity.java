fca1de63d2172780acdde31080b3f4911eed7084
workofp <workofp@163.com>
2017-08-12 10:27:07 +0800

    创建工程

---
+++ app/src/main/java/bookweather/com/bookweather/MainActivity.java
@@ -0,0 +1,13 @@
+package bookweather.com.bookweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
